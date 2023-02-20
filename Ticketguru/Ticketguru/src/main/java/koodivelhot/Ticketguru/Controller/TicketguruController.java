ac8a176690967908454520c3f320db7f62c00e44
huovelinjaakko <jaakkohuovelin@gmail.com>
2023-02-20 16:04:55 +0200

    Pari REST-methodia lisätty, GET events ja GET event by id

--- Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/Controller/TicketguruController.java
+++ Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/Controller/TicketguruController.java
@@ -11,6 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-public class TicketguruController {
+import koodivelhot.Ticketguru.Domain.PlaceOfEvent.Event;
+import koodivelhot.Ticketguru.Domain.PlaceOfEvent.EventRepository;
 
+@Controller
+public class TicketguruController {
+	
+	private EventRepository event_repo;
+	
+	// REST, get all events
+	// Ei vielä testattu, heittää jotain erroreita entiteetteihin liittyen käynnistäessä
+	@RequestMapping(value = "/events", method = RequestMethod.GET)
+	public @ResponseBody List<Event> eventListRest() {
+		return(List<Event>) event_repo.findAll();
+	}
+	
+	// REST, get event by id
+	@RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
+	public @ResponseBody Event findEventRest(@PathVariable("id") Long event_id) {
+		return event_repo.findById(event_id);
+	}
 }
--- Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/Domain/PlaceOfEvent/EventRepository.java
+++ Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/Domain/PlaceOfEvent/EventRepository.java
@@ -8,5 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface EventRepository extends CrudRepository<Event, String> {
 
 	List<Event> findByEventName(String eventName);
+
+	Event findById(Long event_id);
     
 }
