a4dfb6e42e7e898f9546e279f32086759f93c65b
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-03-02 13:49:10 +0200

    REST get, post, put, delete Controller

--- Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/web/TicketguruController.java
+++ Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/web/TicketguruController.java
@@ -6,13 +6,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import koodivelhot.Ticketguru.Domain.AppUser;
 import koodivelhot.Ticketguru.Domain.AppUserRepository;
@@ -34,23 +37,36 @@ public class TicketguruController {
 	}
 	
 	// REST, get all events
-	// Ei vielä testattu, heittää jotain erroreita entiteetteihin liittyen käynnistäessä
 	@RequestMapping(value = "/events", method = RequestMethod.GET)
 	public @ResponseBody List<Event> eventListRest() {
 		return(List<Event>) erepository.findAll();
 	}
 	
 	// REST, get event by id
-	//@RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
-	//public @ResponseBody Event findEventRest(@PathVariable("id") Long event_id) {
-		//return eventRepository.findById(event_id);
-	//}
+	@RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Event> findEventRest(@PathVariable("id") Long event_id) {
+		return erepository.findById(event_id);
+	}
 	
 	// REST, add new event
-	//@PostMapping("events")
-	//public Event newEvent(@RequestBody Event newEvent) {
-		//return eventRepository.save(newEvent);
-	//}
+	@RequestMapping(value = "/events", method = RequestMethod.POST)
+	public @ResponseBody Event newEvent(@RequestBody Event newEvent) {
+		return erepository.save(newEvent);
+	}
+	
+	//REST, update event by id
+	@RequestMapping(value = "/event/{id}", method = RequestMethod.PUT)
+	public @ResponseBody Event editEvent(@RequestBody Event editedEvent, @PathVariable("id") Long event_id) {
+		editedEvent.setEvent_id(event_id);
+		return erepository.save(editedEvent);
+	}
+	
+	//rest, delete by id and show updated list of events
+	@RequestMapping(value = "/event/{id}", method = RequestMethod.DELETE)
+	public @ResponseBody List<Event> deleteEvent(@PathVariable("id") Long event_id) {
+		erepository.deleteById(event_id);
+		return (List<Event>) erepository.findAll();
+	}
 	
 	// REST, get all users
 	
