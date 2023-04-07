229ef1f574658ec976da20c78b907f060cc0a754
perunajenny <jennyberg@Jennys-MBP.lan>
2023-04-07 22:57:18 +0300

    Ennakkolippuun lisätty koodi (kommentoitu pois

--- Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/web/PreSaleTicketController.java
+++ Ticketguru/Ticketguru/src/main/java/koodivelhot/Ticketguru/web/PreSaleTicketController.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,17 @@ public class PreSaleTicketController {
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ennakkolippua ei löytynyt annetulla id:llä");
 		}
 	}
+	
+	// Get presaleticket by code => metodi ennakkolipun hakemiseen koodilla, koodi annetaan parametrina
+	/*@GetMapping(value = "/presaletickets", params = {"code"})
+	public PreSaleTicket findByCode(@RequestParam("code") String code) {
+		PreSaleTicket presaleticket = pstrepository.findByCode(code);
+		
+		if (presaleticket == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Presaleticket with code" + code + "not found");
+		}
+		return presaleticket;
+	}*/
 	
 	// REST, delete presale ticket by id
 	@RequestMapping(value = "/presaleticket/{id}", method = RequestMethod.DELETE)
